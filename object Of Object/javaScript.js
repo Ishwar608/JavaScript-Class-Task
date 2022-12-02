7c713d263243b182f501f2b4769968d621fa9a18
Ishwar <whitehacker608@gmail.com>
2022-12-02 15:13:26 +0530

    add Array Key

--- object Of Object/javaScript.js
+++ object Of Object/javaScript.js
@@ -1,19 +1,31 @@
 fetch("https://jsonplaceholder.typicode.com/users")
 .then(y=>y.json()).then(t=>display(t));
 
+let myJson = [];
+let arrayKey = [];
 display = (data) =>{
-    let kay = Object.keys(data[0]);
+    myJson = data;
+    arrayOfKey();
+    // let kay = Object.keys(myJson[0]);    
 
-    document.getElementById('tblhead').innerHTML += kay.map((tblKey)=>{
-        return `<th>${tblKey}</th>`;
-    }).join("");
+     document.getElementById('tblhead').innerHTML += arrayKey.map((tblKey)=>{
+         return `<th>${tblKey}</th>`;
+     }).join("");
 
-    document.getElementById('tblbody').innerHTML += data.map((tblVal)=>{
+    document.getElementById('tblbody').innerHTML += myJson.map((tblVal)=>{
         let val = '';        
         for (const key in tblVal) {
             if(typeof tblVal[key] === "object"){
                 for (const key1 in tblVal[key]) {
-                val += `<td>${tblVal[key][key1]}</td>`
+                        if(typeof tblVal[key][key1] === "object"){
+                            for (const key3 in tblVal[key][key1]) {
+                            val += `<td>${tblVal[key][key1][key3]}</td>`
+
+                            }
+                        }
+                        else{
+                            val += `<td>${tblVal[key][key1]}</td>`
+                        }
                 }
             }
             else{
@@ -22,4 +34,34 @@ display = (data) =>{
         }
     return `<tr>${val}</tr>`;
     }).join('');
+
+    arrayOfKey();
+}
+
+arrayOfKey = () =>{
+//   arrayKey = Object.keys(myJson[0])
+
+    for (const key in myJson) {
+        if(key == 0){
+            if(typeof myJson[key] === 'object'){
+               
+                for (const key1 in myJson[key]) {
+                    if(typeof myJson[key][key1] === 'object'){
+                        for (const key3 in myJson[key][key1]) {
+                               arrayKey.push(key3);
+                        }
+                    }
+                    else{
+
+                        arrayKey.push(key1);
+                    }
+                }
+            
+            }
+            
+        
+        }
+
+    }
+    // console.log(arrayKey);
 }
